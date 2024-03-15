248a128be96aca39f444a6b9c0ff3da6bfe25429
Serikbol <serikboldany@gmail.com>
2024-03-15 17:01:52 +0500

    third commit

--- src/main/java/com/company/sprintSp2/controllers/MainController.java
+++ src/main/java/com/company/sprintSp2/controllers/MainController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -15,6 +18,8 @@ public class MainController {
     private RequestsRepository requestsRepository;
     @GetMapping(value = "/")
     public String index(Model model){
+        List<ApplicationRequest> requests = requestsRepository.findAll();
+        model.addAttribute("req", requests);
         return "index";
     }
 
@@ -32,7 +37,44 @@ public class MainController {
         applicationRequest.setCourseName(course);
         applicationRequest.setPhone(phoneNumber);
         applicationRequest.setCommentary(commentary);
+        applicationRequest.setHandled(true);
+
         requestsRepository.save(applicationRequest);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/newR")
+    public String newR(Model model){
+        List<ApplicationRequest> requests = requestsRepository.findAll();
+        model.addAttribute("req", requests);
+        return "newReq";
+    }
+
+    @GetMapping(value = "/proR")
+    public String proR(Model model){
+        List<ApplicationRequest> requests = requestsRepository.findAll();
+        model.addAttribute("req", requests);
+        return "proReq";
+    }
+
+    @GetMapping(value = "/details/{requestId}")
+    public String details(Model model, @PathVariable(name = "requestId") Long id){
+        ApplicationRequest applicationRequest = requestsRepository.findById(id).orElse(null);
+        model.addAttribute("req", applicationRequest);
+        return "details";
+    }
+
+    @PostMapping(value = "/reHandle")
+    public String reHandle(@RequestParam(name = "id") Long id){
+        ApplicationRequest applicationRequest = requestsRepository.findById(id).orElse(null);
+        applicationRequest.setHandled(false);
+        System.out.println(applicationRequest.isHandled() + " " + applicationRequest.getId());
+        return "redirect:/";
+    }
+    @PostMapping(value = "/delete")
+    public String delete(@RequestParam(name = "id") Long id){
+        ApplicationRequest applicationRequest = requestsRepository.findById(id).orElse(null);
+        requestsRepository.delete(applicationRequest);
         return "redirect:/";
     }
 }
